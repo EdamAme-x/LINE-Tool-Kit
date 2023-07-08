e5914be8ddc8d9ebd9c4f1f0071603f0a9f236a0
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-07-08 11:51:53 +0900

    add

--- LINE-Tool-Kit/serve.ts
+++ LINE-Tool-Kit/serve.ts
@@ -1,11 +1,11 @@
-import { serve } from 'https://deno.land/std/http/server.ts'
-import { Hono } from 'https://deno.land/x/hono/mod.ts'
-import { serveStatic } from 'https://deno.land/x/hono/middleware.ts'
+import { serve } from 'https://deno.land/std/http/server.ts';
+import { Hono } from 'https://deno.land/x/hono/mod.ts';
+import { serveStatic } from 'https://deno.land/x/hono/middleware.ts';
 
-const app = new Hono()
+const app = new Hono();
 
-app.use('/*', serveStatic({ root: './dist' }))
-app.use('/favicon.ico', serveStatic({ path: './public/vite.ico' }))
-app.get('/', serveStatic({ path: './dist/index.html' }))
+app.use('/*', serveStatic({ root: './dist' }));
+app.use('/favicon.ico', serveStatic({ path: './public/vite.ico' }));
+app.use('/', serveStatic({ path: './dist/index.html' }));
 
-serve(app.fetch)
+serve(app.fetch.bind(app));
